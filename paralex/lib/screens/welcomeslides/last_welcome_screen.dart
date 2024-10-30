937ac57c043e478b6832c9d543f6e9f5b6bb56a5
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2024-10-30 09:24:57 +0100

    new file:   paralex/assets/images/Emoji.svg 	modified:   paralex/lib/main.dart 	modified:   paralex/lib/reusables/paints.dart 	modified:   paralex/lib/routes/navs.dart 	new file:   paralex/lib/screens/users/Authprocess/otp.dart 	new file:   paralex/lib/screens/users/Authprocess/registration.dart 	new file:   paralex/lib/screens/users/Authprocess/verification_message.dart 	modified:   paralex/lib/screens/welcomeslides/last_welcome_screen.dart 	modified:   paralex/lib/screens/welcomeslides/welcome.dart 	modified:   paralex/pubspec.lock 	modified:   paralex/pubspec.yaml

--- paralex/lib/screens/welcomeslides/last_welcome_screen.dart
+++ paralex/lib/screens/welcomeslides/last_welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:paralax/reusables/fonts.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:paralax/routes/navs.dart';
 
 class LastWelcomeScreen extends StatelessWidget {
   const LastWelcomeScreen({super.key});
@@ -62,7 +64,7 @@ class LastWelcomeScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   InkWell(
-                    onTap: (){},
+                    onTap: () => Get.toNamed(Nav.userSignupScreen),
                     child: const ParticipantWidget(
                       imgPath: "assets/images/2user.svg",
                       firstText: "User",
@@ -72,7 +74,7 @@ class LastWelcomeScreen extends StatelessWidget {
                   ),
                   const SizedBox(width: 35),
                   InkWell(
-                    onTap: (){},
+                    onTap: () {},
                     child: const ParticipantWidget(
                       imgPath: "assets/images/medal-star.svg",
                       firstText: "Service Provider",
@@ -96,11 +98,13 @@ class LastWelcomeScreen extends StatelessWidget {
                         .copyWith(letterSpacing: 0, fontSize: 14),
                   ),
                   InkWell(
-                    onTap: (){},
+                    onTap: () {},
                     child: Text(
                       "Sign in",
                       style: FontStyles.smallCapsIntro.copyWith(
-                          letterSpacing: 0, fontWeight: FontWeight.w900,fontSize: 14),
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 14),
                     ),
                   )
                 ],
@@ -148,7 +152,7 @@ class ParticipantWidget extends StatelessWidget {
             ),
           ),
           Transform.translate(
-            offset: const Offset(0,-5),
+            offset: const Offset(0, -5),
             child: Text(
               thirdText,
               style: FontStyles.smallCapsIntro.copyWith(

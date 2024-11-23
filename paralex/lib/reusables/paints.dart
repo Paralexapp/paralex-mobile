3d90994c31fd30fbdbcd563a5765fce222079956
Jonny <adigunjohn5028@gmail.com>
2024-11-23 19:15:29 +0100

    logistics f

--- paralex/lib/reusables/paints.dart
+++ paralex/lib/reusables/paints.dart
@@ -15,6 +15,13 @@ class PaintColors {
   static const cancelTextColor = Color(0xFF21252C);
   static const paralexLightGrey = Color(0xFFD9D9D9);
   static const paralexVeryLightGrey = Color(0xFFECF1F4);
+  static const paralexGreyShade1 = Color(0xFFfAFCFE);
+  static const paralexGreyShade2 = Color(0xFFECF0F4);
+  static const paralexGreyShade3 = Color(0xFFF0F5FA);
+  static const paralexGreyShade4 = Color(0xFFF7F8F9);
+  static const paralexTextColor1 = Color(0xFF181C2E);
+  static const paralexTextColor2 = Color(0xFF464E57);
+
   static const bodyText1Color = Color(0xFF8C8CA1);
   static const textFieldBorderColor = Color(0xFF31D0AA);
   static const white = Colors.white;
